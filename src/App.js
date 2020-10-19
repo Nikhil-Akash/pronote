bf51f154178ed17a2013b95b4d8c44d1a31bd708
Nikhil Akash <samineninikhilakash@gmail.com>
2020-10-19 23:32:31 +0530

    pronote

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Editor from './components/Editor'
+import ProNote from './components/ProNote'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+
+    constructor() {
+        super()
+        this.state = {
+            value: " "
+        }
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange(e) {
+        this.setState(
+            { value: e.target.value }
+        )
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <Editor handleChange={this.handleChange} />
+                <ProNote state={this.state} />
+            </React.Fragment>
+        )
+    }
 }
-
-export default App;
